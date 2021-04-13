7518a881acd7628d0f1f0dcddf7ea1bbd0076755
Yuri Kuznetsov <60663886+kuz-sysadmin@users.noreply.github.com>
2021-04-13 17:28:55 -0400

    translate to english

--- sub/bbsadmin.php
+++ sub/bbsadmin.php
@@ -1,388 +1,115 @@
-<?php
-
-/*
-
-くずはすくりぷとPHP ver0.0.7alpha (13:04 2003/02/18)
-管理モードモジュール
-
-*/
-
-if(!defined("INCLUDED_FROM_BBS")) {
-    header ("Location: ../bbs.php");
-    exit();
-}
-
-
-
-/**
- * 管理モードモジュール
- *
- *
- *
- * @package strangeworld.cnscript
- * @access  public
- */
-class Bbsadmin extends Webapp {
-
-    var $bbs;
-
-    /**
-     * コンストラクタ
-     *
-     */
-    function Bbsadmin() {
-        parent::__construct();
-        if (func_num_args() > 0) {
-            $this->bbs = func_get_arg(0);
-            $this->c = &$this->bbs->c;
-            $this->f = &$this->bbs->f;
-            $this->t = &$this->bbs->t;
-        }
-        $this->t->readTemplatesFromFile($this->c['TEMPLATE_ADMIN']);
-    }
-
-
-    /**
-     * メイン処理
-     */
-    function main() {
-
-        if (!defined('BBS_ACTIVATED')) {
-
-            # 実行時間測定開始
-            $this->setstarttime();
-
-            # フォーム取得前処理
-            $this->procForm();
-
-            # 個人用設定反映
-            $this->refcustom();
-            $this->setusersession();
-
-            # gzip圧縮転送
-            if ($this->c['GZIPU']) {
-                ob_start("ob_gzhandler");
-            }
-        }
-
-        # ログファイル閲覧
-        if (@$this->f['ad'] == 'l') {
-            $this->prtlogview(TRUE);
-        }
-        # メッセージ削除モード
-        else if (@$this->f['ad'] == 'k') {
-            $this->prtkilllist();
-        }
-        # メッセージ削除処理
-        else if (@$this->f['ad'] == 'x') {
-            if (isset($this->f['x'])) {
-                $this->killmessage($this->f['x']);
-            }
-            $this->prtkilllist();
-        }
-        # 暗号化パスワード生成画面
-        else if (@$this->f['ad'] == 'p') {
-            $this->prtsetpass();
-        }
-        # 暗号化パスワード生成＆表示
-        else if (@$this->f['ad'] == 'ps') {
-            $this->prtpass(@$this->f['ps']);
-        }
-        # サーバーのPHP設定情報表示
-        else if (@$this->f['ad'] == 'phpinfo') {
-            phpinfo();
-        }
-        # 管理メニュー画面
-        else {
-            $this->prtadminmenu();
-        }
-
-
-        if (!defined('BBS_ACTIVATED') and $this->c['GZIPU']) {
-            ob_end_flush();
-        }
-    }
-
-
-
-
-
-    /**
-     * 管理メニュー画面
-     *
-     */
-    function prtadminmenu() {
-
-        $this->t->addVar('adminmenu', 'V', trim($this->f['v']));
-
-        $this->sethttpheader();
-        print $this->prthtmlhead ($this->c['BBSTITLE'] . ' 管理メニュー');
-        $this->t->displayParsedTemplate('adminmenu');
-        print $this->prthtmlfoot ();
-
-    }
-
-
-
-
-
-    /**
-     * メッセージ削除モードメイン画面表示
-     *
-     */
-    function prtkilllist() {
-
-        if (!file_exists($this->c['LOGFILENAME'])) {
-            $this->prterror('メッセージ読み込みに失敗しました');
-        }
-        $logdata = file($this->c['LOGFILENAME']);
-
-        $this->t->addVar('killlist', 'V', trim($this->f['v']));
-
-        $messages = array();
-        while ($logline = each($logdata)) {
-            $message = $this->getmessage($logline[1]);
-            $message['MSG'] = preg_replace("/<a href=[^>]+>参考：[^<]+<\/a>/i", "", $message['MSG'], 1);
-            $message['MSG'] = preg_replace("/<[^>]+>/", "", ltrim($message['MSG']));
-            $msgsplit = explode("\r", $message['MSG']);
-            $message['MSGDIGEST'] = $msgsplit[0];
-            $index = 1;
-            while ($index < count($msgsplit) - 1 and strlen($message['MSGDIGEST'] . $msgsplit[$index]) < 50) {
-                $message['MSGDIGEST'] .= $msgsplit[$index];
-                $index++;
-            }
-            $message['WDATE'] = Func::getdatestr($message['NDATE']);
-            $message['USER_NOTAG'] = preg_replace("/<[^>]*>/", '', $message['USER']);
-            $messages[] = $message;
-        }
-
-        $this->t->addRows('killmessage', $messages);
-
-        $this->sethttpheader();
-        print $this->prthtmlhead ($this->c['BBSTITLE'] . ' メッセージ削除モード');
-        $this->t->displayParsedTemplate('killlist');
-        print $this->prthtmlfoot ();
-    }
-
-
-
-
-
-    /**
-     * メッセージ削除処理
-     *
-     */
-    function killmessage($killids) {
-
-        if (!$killids) {
-            return;
-        }
-        if (!is_array($killids)) {
-            $tmp = $killids;
-            $killids = array();
-            $killids[] = $tmp;
-        }
-
-        $fh = @fopen($this->c['LOGFILENAME'], "r+");
-        if (!$fh) {
-            $this->prterror ( 'メッセージ読み込みに失敗しました' );
-        }
-        flock ($fh, 2);
-        fseek ($fh, 0, 0);
-
-        $logdata = array();
-        while (($logline = Func::fgetline($fh)) !== FALSE) {
-             $logdata[] = $logline;
-        }
-
-        $killntimes = array();
-        $killlogdata = array();
-        $newlogdata = array();
-        $i = 0;
-        while ($i < count($logdata)) {
-            $items = explode(',', $logdata[$i], 3);
-            if (count($items) > 2 and array_search($items[1], $killids) !== FALSE) {
-                $killntimes[$items[1]] = $items[0];
-                $killlogdata[] = $logdata[$i];
-            }
-            else {
-                $newlogdata[] = $logdata[$i];
-            }
-            $i++;
-        }
-        {
-            fseek ($fh, 0, 0);
-            ftruncate ($fh, 0);
-            fwrite ($fh, implode ('', $newlogdata));
-        }
-        flock ($fh, 3);
-        fclose ($fh);
-
-        # 画像削除
-        foreach ($killlogdata as $eachlogdata) {
-            if (preg_match("/<img [^>]*?src=\"([^\"]+)\"[^>]+>/i", $eachlogdata, $matches) and file_exists($matches[1])) {
-                unlink ($matches[1]);
-            }
-        }
-
-        # 過去ログ行削除
-        if ($this->c['OLDLOGFILEDIR']) {
-            foreach (array_keys($killntimes) as $killid) {
-                $oldlogfilename = '';
-                if ($this->c['OLDLOGFMT']) {
-                    $oldlogext = 'dat';
-                }
-                else {
-                    $oldlogext = 'html';
-                }
-                if ($this->c['OLDLOGSAVESW']) {
-                    $oldlogfilename = date("Ym", $killntimes[$killid]) . ".$oldlogext";
-                }
-                else {
-                    $oldlogfilename = date("Ymd", $killntimes[$killid]) . ".$oldlogext";
-                }
-                $fh = @fopen($this->c['OLDLOGFILEDIR'] . $oldlogfilename, "r+");
-                if ($fh) {
-                    flock ($fh, 2);
-                    fseek ($fh, 0, 0);
-
-                    $newlogdata = array();
-                    $hit = FALSE;
-
-                    if ($this->c['OLDLOGFMT']) {
-                        $needle = $killntimes[$killid] . "," . $killid . ",";
-                        while (($logline = Func::fgetline($fh)) !== FALSE) {
-                            if (!$hit and strpos($logline, $needle) !== FALSE and strpos($logline, $needle) == 0) {
-                                $hit = TRUE;
-                            }
-                            else {
-                                $newlogdata[] = $logline;
-                            }
-                        }
-                    }
-                    else {
-                        $needle = "<div class=\"m\" id=\"m{$killid}\">";
-                        $flgbuffer = FALSE;
-                        while (($htmlline = Func::fgetline($fh)) !== FALSE) {
-
-                            # メッセージの開始
-                            if (!$hit and strpos($htmlline, $needle) !== FALSE) {
-                                $hit = TRUE;
-                                $flgbuffer = TRUE;
-                            }
-                            # メッセージの終了
-                            else if ($flgbuffer and strpos($htmlline, "<hr") !== FALSE) {
-                                $flgbuffer = FALSE;
-                            }
-                            # メッセージ中
-                            else if ($flgbuffer) {
-                            }
-                            else {
-                                $newlogdata[] = $htmlline;
-                            }
-                        }
-                    }
-
-                    {
-                        fseek ($fh, 0, 0);
-                        ftruncate ($fh, 0);
-                        fwrite ($fh, implode ('', $newlogdata));
-                    }
-                    flock ($fh, 3);
-                    fclose ($fh);
-                }
-                else {
-                    #$this->prterror ( '過去ログ読み込みに失敗しました' );
-                }
-            }
-        }
-
-    }
-
-
-
-
-
-    /**
-     * 暗号化パスワード生成画面表示
-     *
-     */
-    function prtsetpass() {
-
-        $this->t->addVar('setpass', 'V', trim($this->f['v']));
-
-        $this->sethttpheader();
-        print $this->prthtmlhead ($this->c['BBSTITLE'] . ' パスワード設定画面');
-        $this->t->displayParsedTemplate('setpass');
-        print $this->prthtmlfoot ();
-    }
-
-
-
-
-
-    /**
-     * 暗号化パスワード生成＆表示
-     *
-     */
-    function prtpass($inputpass) {
-
-        if (!@$inputpass) {
-            $this->prterror ('パスワードが設定されていません。');
-        }
-
-        $cryptpass = crypt($inputpass);
-        $inputsize = strlen($cryptpass) + 10;
-
-        $this->t->addVars('pass', array(
-            'CRYPTPASS' => $cryptpass,
-            'INPUTSIZE' => $inputsize,
-        ));
-
-        $this->sethttpheader();
-        print $this->prthtmlhead ($this->c['BBSTITLE'] . ' パスワード設定画面');
-        $this->t->displayParsedTemplate('pass');
-        print $this->prthtmlfoot ();
-    }
-
-
-
-
-
-    /**
-     * ログファイル表示
-     *
-     */
-    function prtlogview($htmlescape = FALSE) {
-        if ($htmlescape) {
-            header ("Content-type: text/html");
-            $logdata = file ($this->c['LOGFILENAME']);
-            print "<html><head><title>{$this->c['LOGFILENAME']}</title></head><body><pre>\n";
-            foreach ($logdata as $logline) {
-                if (!preg_match("/^\w+$/", $logline)) {
-                    #$value_euc = JcodeConvert($logline, 2, 1);
-                    #$value_euc = htmlentities($value_euc, ENT_QUOTES, 'EUC-JP');
-                    #$logline = JcodeConvert($value_euc, 1, 2);
-                    $logline = htmlspecialchars($logline, ENT_QUOTES);
-                }
-                $logline = str_replace("&#44;", ",", $logline);
-                print $logline;
-            }
-            print "\n</pre></body></html>";
-        }
-        else {
-            header ("Content-type: text/plain");
-            readfile ($this->c['LOGFILENAME']);
-        }
-    }
-
-
-
-
-
-
-}
-
-
-
-?>
+<!-- くずはすくりぷとPHP  HTMLテンプレート  管理モード用 -->
+<!-- 管理メニュー画面 -->
+<patTemplate:tmpl name="adminmenu">
+<header><span class="pagetitle"><a href="{CGIURL}" style="text-decoration: none;">{BBSTITLE}</a></span> <a href="{DEFURL}">←bbs</a></header>
+<h1>Administration Menu</h1>
+<p><strong>Warning:</strong> Unauthorized access to the admin menu is strictly prohibited.Violators will be tracked, identified and detained.</p>
+<form method="post" action="{CGIURL}">
+	<input type="hidden" name="m" value="ad" />
+	<input type="hidden" name="ad" value="k" />
+	<input type="hidden" name="u" value="{U}" />
+	<input type="hidden" name="v" value="{V}" />
+	<input type="submit" value="Delete Message" class="btn" />
+</form>
+<form method="post" action="{CGIURL}">
+	<input type="hidden" name="m" value="ad" />
+	<input type="hidden" name="ad" value="l" />
+	<input type="hidden" name="u" value="{U}" />
+	<input type="hidden" name="v" value="{V}" />
+	<input type="submit" value="Log File browsing" class="btn" />
+</form>
+<form method="post" action="{CGIURL}">
+	<input type="hidden" name="m" value="ad" />
+	<input type="hidden" name="ad" value="p" />
+	<input type="hidden" name="u" value="{U}" />
+	<input type="hidden" name="v" value="{V}" />
+	<input type="submit" value="Encrypted password regeneration" class="btn" />
+</form>
+<form method="post" action="{CGIURL}">
+	<input type="hidden" name="m" value="ad" />
+	<input type="hidden" name="ad" value="phpinfo" />
+	<input type="hidden" name="u" value="{U}" />
+	<input type="hidden" name="v" value="{V}" />
+	<input type="submit" value="Server PHP Configuration Information" class="btn" />
+</form>
+<br>
+<form method="post" action="{CGIURL}">
+	<input type="submit" value="End" class="btn" />
+</form>
+</patTemplate:tmpl>
+
+<!-- メッセージ削除モードメイン画面 -->
+<patTemplate:tmpl name="killlist">
+<header><span class="pagetitle"><a href="{CGIURL}" style="text-decoration: none;">{BBSTITLE}</a></span></header>
+<h1>Message Deletion Mode</h1>
+<form method="post" action="{CGIURL}">
+	<input type="hidden" name="m" value="ad" />
+	<input type="hidden" name="u" value="{U}" />
+	<input type="hidden" name="v" value="{V}" />
+	<a href="{DEFURL}">bbs</a>
+	<input type="button" onclick="location.href='#bottom'" accesskey="B" value="▼" class="btn" title="Alt(+Shift)+B">
+</form>
+<form method="post" action="{CGIURL}">
+	<input type="hidden" name="m" value="ad" />
+	<input type="hidden" name="ad" value="x" />
+	<input type="hidden" name="u" value="{U}" />
+	<input type="hidden" name="v" value="{V}" />
+	<p>Check the post you want to delete and press the "Delete Run" button at the bottom.</p>
+	<hr>
+	<span class="medium">Delete-Display-Posting date-Title-Contributor-Content (some）</span>
+	<pre>
+<patTemplate:tmpl name="killmessage">
+<input type="checkbox" name="x[]" value="{POSTID}" /> - <a href="{CGIURL}?m=f&amp;s={POSTID}" target="link">{POSTID}</a> - [{WDATE}] - {TITLE} - {USER_NOTAG} - {MSGDIGEST}
+</patTemplate:tmpl>
+</pre>
+<hr>
+<input type="submit" value="　[!]Run Delete　" class="btn" />
+</form>
+<form method="post" action="{CGIURL}">
+	<input type="hidden" name="m" value="ad" />
+	<input type="hidden" name="u" value="{U}" />
+	<input type="hidden" name="v" value="{V}" />
+	<a href="{DEFURL}">bbs</a>
+	<input type="button" onclick="location.href='#TOP'" accesskey="T" value="▲" class="btn" title="Alt(+Shift)+T"></form>
+</patTemplate:tmpl>
+
+<!-- 暗号化パスワード生成画面 -->
+<patTemplate:tmpl name="setpass">
+<header><span class="pagetitle">{BBSTITLE}</span> - password setting screen<header><br>
+<form method="post" action="{CGIURL}">
+	<input type="hidden" name="m" value="ad" />
+	<input type="hidden" name="ad" value="ps" />
+	<input type="hidden" name="u" value="{U}" />
+	<input type="hidden" name="v" value="{V}" />
+<div class = "bbsmsg"> Set the password.<br>
+Please enter the"Administrative password"to be used in the management of the bulletin board from now on.<br>
+The password you enter here is used for posting with the administrator's name and authentication in admin mode.</div>
+	<br><br>
+	<table border="2" cellspacing="4">
+		<tr>
+			<td align="center">Administrative password</td>
+			<td align="center"><input size="30" type="text" name="ps" maxlength="127" value="" class="text" /></td>
+		</tr>
+	</table>
+	<br><br>
+	<input type="submit" value="Settings" class="btn" />　<input type="reset" value="Reset" class="btn" />
+</form>
+</patTemplate:tmpl>
+
+<!-- 暗号化パスワード表示画面 -->
+<patTemplate:tmpl name="pass">
+<header><span class="pagetitle">{BBSTITLE}</span> - Password setting screen</header>
+<form method="post" action="{CGIURL}">
+	<div class="bbsmsg">We have generated an encrypted password!<br>
+	Paste the following encrypted password string into the place of the bulletin board script body.</div>
+	<br><br>
+	<table border="2" cellspacing="4">
+		<tr>
+			<td align="center">Administrative password</td>
+			<td align="center"><input size="{INPUTSIZE}" type="text" name="cp" value="{CRYPTPASS}" readonly="readonly" class="text" /></td>
+		</tr>
+	</table>
+	<br><br>
+	<a href="{CGIURL}">bbs</a>
+</form>
+</patTemplate:tmpl>
